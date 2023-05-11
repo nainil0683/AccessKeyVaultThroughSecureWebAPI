bcfd5c86addaa8da828c4bdd4c4978e2e6c46b5b
nainil0683 <nainil0683@hotmail.com>
2023-05-11 11:42:52 -0400

    Commiting the first code

---
+++ Controllers/KeyVaultController.cs
@@ -0,0 +1,44 @@
+﻿using Azure;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AccessKeyVaultThroughHttpsWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class KeyVaultController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public KeyVaultController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetKeyVaultSecret")]
+        public string Get()
+        {
+            string stringToReturn = "AanyaKiara";
+
+            try
+            {
+
+                DefaultAzureCredential defaultAzureCredential = new DefaultAzureCredential();
+
+                Uri keyVaultUri = new Uri("https://testkeyvalultak1.vault.azure.net/");
+                SecretClient secretClient = new SecretClient(keyVaultUri, defaultAzureCredential);
+
+                Response<KeyVaultSecret> response = secretClient.GetSecret("SqlServerConnectionString");
+
+                stringToReturn = response.Value.Value;
+            }
+            catch (Exception ex)
+            {
+                stringToReturn = ex.ToString();
+            }
+
+            return stringToReturn;
+        }
+    }
+}
